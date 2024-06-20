ecbb074aa2bf171156e260207e66e7a74dcbd24b
manager6 <2358735845@qq.com>
2024-06-20 13:40:41 +0800

    系统框架搭建

---
+++ 代码/系统框架搭建/example.c
@@ -0,0 +1,6 @@
+#include "example.h"
+#include <stdio.h>
+
+void myFunction(int a, int b) {
+    printf("a + b = %d\n", a + b);
+}
---
+++ 代码/系统框架搭建/main.c
@@ -0,0 +1,6 @@
+#include "example.h"
+
+int main() {
+    myFunction(1, 2);
+    return 0;
+}
